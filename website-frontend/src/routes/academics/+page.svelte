bd7846de21b309a2618c62e17e70ad7340012a32
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2025-01-17 20:58:58 +0800

    fix: add banner component as navbar buffer

--- website-frontend/src/routes/academics/+page.svelte
+++ website-frontend/src/routes/academics/+page.svelte
@@ -1,11 +1,17 @@
 <script>
 	/** @type {import('./$types').PageData} */
+	import Banner from '$lib/components/banner/Banner.svelte';
 	export let data;
+
+	const title = 'Academics';
 </script>
 
-<h1>Academics</h1>
-
 <ul>
+	<Banner
+		{title}
+		background_image={''}
+		flexible_content={`This is the ${title} page.`}
+	/>
 	{#each data.programs as { slug, title }}
 		<li><a href="/academics/{slug}">{title}</a></li>
 	{/each}
