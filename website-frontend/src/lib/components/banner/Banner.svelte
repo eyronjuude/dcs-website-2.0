4470441c25d9a5e08046d9bea14aaf288af51765
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2025-01-17 20:53:15 +0800

    style(Banner): generalize title banners

--- website-frontend/src/lib/components/banner/Banner.svelte
+++ website-frontend/src/lib/components/banner/Banner.svelte
@@ -3,16 +3,26 @@
 	import { PUBLIC_APIURL } from '$env/static/public';
 
 	export let title: string;
-	export let background_image: string;
+	export let background_image: string = '';
 	export let flexible_content: string;
-	export let text_bottom: number = 24;
+	export let from_bottom: number = NaN;
+
+	$: text_bottom = from_bottom;
+
+	$: if (from_bottom !== from_bottom) {
+		text_bottom = ((flexible_content) ? 24 : 9);
+	}
+
+	$: height = ((flexible_content) ? 45 : 40);
+	$: max_height = ((flexible_content) ? 60 : 50);
+	$: font_size = ((flexible_content) ? 4 : 5);
 
 	$: deslugify_title = deslugify(title);
 </script>
 
 <div class="relative z-0">
 	<div
-		class="h-[45vh] bg-cover bg-center md:h-[60vh]"
+		class="h-[{height}vh] bg-cover bg-center md:h-[{max_height}vh]"
 		style="background-image: linear-gradient(to top, hsl(var(--primary)), transparent), url('{PUBLIC_APIURL}/assets/{background_image}')"
 	></div>
 
@@ -21,7 +31,9 @@
         -translate-x-1/2 transform text-center text-primary-foreground
         md:left-0 md:max-w-[60vw] md:translate-x-0 md:px-32 md:text-start"
 	>
-		<h1 class="text-3xl font-bold md:mb-4 md:text-4xl">{deslugify_title}</h1>
-		<h4 class="hidden text-muted md:block">{@html flexible_content}</h4>
+		<h1 class="text-3xl font-bold md:text-{font_size}xl">{deslugify_title}</h1>
+		{#if flexible_content}
+			<h4 class="hidden text-muted md:block md:mt-4">{@html flexible_content}</h4>
+		{/if}
 	</div>
 </div>
